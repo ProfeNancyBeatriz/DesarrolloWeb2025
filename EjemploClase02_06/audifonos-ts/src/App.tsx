6e9415012ee44f7ac4365d564599d671bb53e1d8
ProfeNancyBeatriz <nancy.bernal@usm.cl>
2025-06-02 10:39:10 -0400

    Ejemplo audifonos con carrito
    Se agregó carrito de compras con reduce

---
+++ EjemploClase02_06/audifonos-ts/src/App.tsx
@@ -0,0 +1,43 @@
+import { useReducer } from "react"
+
+import Audifono from "./components/Audifono"
+import Header from "./components/Header"
+import { cartReducer, initialState } from "./reducers/cart-reducer"
+
+function App() {
+
+  //const [data, setData] = useState(db)
+
+  const [state, dispatch] = useReducer(cartReducer,initialState)
+  return (    
+    <>
+      <Header
+        cart={state.cart}
+        dispatch={dispatch}
+      />
+      <main className="container-xl mt-5">
+        <h2 className="text-center">Nuestra Colección</h2>
+        <div className="row mt-5">
+          {state.data.map((audifono)=>
+          (
+            <Audifono 
+            key={audifono.id}
+            audifono={audifono}
+            dispatch={dispatch}
+            />
+          ))}
+          
+        </div>
+      </main>
+
+
+      <footer className="bg-dark mt-5 py-5">
+        <div className="container-xl">
+          <p className="text-white text-center fs-4 mt-4 m-md-0">Logitesshh - Todos los derechos Reservados</p>
+        </div>
+      </footer>
+    </>
+  )
+}
+
+export default App
